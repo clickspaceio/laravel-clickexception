23df7bd733252fd88848b6c034af4d3198293fb1
Luís Victor Quintas <luisvictorquintas@gmail.com>
2020-04-13 16:06:52 -0300

    add ModelConflictValidationException

---
+++ src/Exceptions/ModelConflictValidationException.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace App\Exceptions;
+
+
+use Clickspace\LaravelClickException\ClickException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+class ModelConflictValidationException extends ClickException
+{
+
+    public $responseBodyType;
+    public $responseBodyCode;
+    public $responseBodyMessage;
+    public $responseBodyAppends;
+    public $responseBodyHttpCode;
+    public $responseHeaders;
+    public $message;
+
+    public function __construct($obj, ModelNotFoundException $previous = null)
+    {
+        $this->responseBodyType = 'invalid_request';
+        $this->responseBodyCode = 'validation_error';
+
+        $this->responseBodyMessage = 'The object informed is already registered.';
+        $this->responseBodyAppends = [
+            'object' => $obj
+        ];
+        $this->responseBodyHttpCode = "409";
+        $this->responseHeaders = [];
+        $this->message = 'vaquinha';
+
+//        parent::__construct($this->message, $this->responseBodyHttpCode, $previous);
+    }
+
+}
